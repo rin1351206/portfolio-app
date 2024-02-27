886087333f81f0fda7e4f5cc56df9e84feca044d
rin1351206 <rin.n.b.1351206@gmail.com>
2024-02-27 23:56:23 +0900

    Material UIの導入成功

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import BlogPage from './components/General/BlogPage';
 import Header from './components/General/Header';
 import HomePage from './components/General/HomePage';
 import TodosPage from './components/Todos/TodosPage';
-import LoginPage from './components/LoginPage';
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/blog" element={<BlogPage />} />
         <Route path='/todos' element={<TodosPage />} />
-        <Route path='/login' element={<LoginPage />} />
       </Routes>
     </div>
     </Router>
